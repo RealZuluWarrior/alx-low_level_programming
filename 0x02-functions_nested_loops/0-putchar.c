ae75bf76857a5582d357cd0feb7d30b647b7bb23
Sanele Chiliza <chilizasanele@gmail.com>
2023-02-21 10:02:48 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "_putchar.c"
+
+/**
+ * main - print _putchar
+ * The character to print
+ *
+ * Return: On success 1.
+ */
+
+int main(void)
+{
+	write("_putchar");
+	return (0);
+}
