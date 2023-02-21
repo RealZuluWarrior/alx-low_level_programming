6370f6d657cab5bc3028c4a441be372242bef3fe
Sanele Chiliza <chilizasanele@gmail.com>
2023-02-21 13:44:49 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* 3-islower : a function print alphabet 10x
+*
+* Return: Always 0 (Succes)
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+return;
+}
